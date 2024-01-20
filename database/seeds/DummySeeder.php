d0d6611866d1641d10fff99371a5d87b571c4136
hossam hassan <hossamhassan889@gmail.com>
2024-01-20 12:34:52 +0200

    vaeerer

--- database/seeds/DummySeeder.php
+++ database/seeds/DummySeeder.php
@@ -1014,8 +1014,8 @@ class DummySeeder extends Seeder
             array('id' => '997','room_name' => 'مكتب  مدير إدارة التخطيط ','item_name' => 'تكيف 2.25 حصان','room_number' => 'C18','floor' => 'السادس','status' => 'wait','created_at' => '0000-00-00 00:00:00.000000','updated_at' => '0000-00-00 00:00:00.000000','deleted_at' => NULL),
             array('id' => '998','room_name' => 'مكتب  مدير إدارة التخطيط ','item_name' => 'IP Phone Avaya J169','room_number' => 'C18','floor' => 'السادس','status' => 'wait','created_at' => '0000-00-00 00:00:00.000000','updated_at' => '2024-01-19 23:33:20.000000','deleted_at' => NULL),
             array('id' => '999','room_name' => 'مكتب  مدير إدارة التخطيط ','item_name' => 'Monitor Samsung Curved 24"','room_number' => 'C18','floor' => 'السادس','status' => 'wait','created_at' => '0000-00-00 00:00:00.000000','updated_at' => '2024-01-19 23:33:20.000000','deleted_at' => NULL)
-        ));
-
+        ) 
+            );
 
     }
 }
